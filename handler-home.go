f5166eff8526f3e76ae3ba41b8bc381fbe668efb
anhdvu <mrandyvu@gmail.com>
2023-08-01 13:49:07 +0700

    added route for klv handler.

--- handler-home.go
+++ handler-home.go
@@ -7,12 +7,14 @@ import "net/http"
 func (app *application) homeHandler() http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		payload := jsonResponse{
-			"GET -> /":                     "To view endpoint information",
-			"POST -> /companion":           "To receive Companion Remote API calls",
-			"POST -> /remote":              "To receive Remote Messageing API calls",
-			"GET -> /companion/responses":  "To view current response codes to Companion Remote API calls",
-			"PUT ->  /companion/responses": "To update one or multiple response codes to Companion Remote API calls",
-			"GET -> /api/logs":             "To view API logs",
+			"GET /":                     "To view endpoint information",
+			"POST /companion":           "To receive Companion Remote API calls",
+			"POST /remote":              "To receive Remote Messageing API calls",
+			"GET /companion/responses":  "To view current response codes to Companion Remote API calls",
+			"PUT /companion/responses":  "To update one or multiple response codes to Companion Remote API calls",
+			"GET /klv?string=xxxyyyzzz": "To analyze a KLV string",
+			"GET /api/logs":             "To view API logs",
+			"GET /api/health":           "To check whether the service is running",
 		}
 
 		err := app.respondJSON(w, http.StatusOK, payload)
--- routes.go
+++ routes.go
@@ -6,12 +6,12 @@ func (app *application) routes() *http.ServeMux {
 	mux := http.NewServeMux()
 
 	mux.Handle("/companion/responses", app.companionResponsesHandler())
-
 	mux.Handle("/companion", app.enforcePOSTRequest(app.enforceXMLHeader(app.XMLRequestHandler())))
 	mux.Handle("/remote", app.enforcePOSTRequest(app.enforceJSONHeader(app.JSONRequestHandler())))
 
-	mux.Handle("/api/health", app.healthz())
+	mux.Handle("/klv", app.klvHandler())
 
+	mux.Handle("/api/health", app.healthz())
 	mux.Handle("/api/logs", app.viewLogsHandler())
 
 	mux.Handle("/", app.homeHandler())
